45239f753273bba0e1e3da11e46625123866f26b
남형석 <azerc@MacMini.local>
2022-08-11 12:47:20 +0900

    .. fix textlist.js export issue

---
+++ src/Components/TextList.js
@@ -0,0 +1,17 @@
+const text_01 = {
+  title: "Beauty",
+  paragraph:
+    "모든 플랫폼의 사진 치수를 기억해야 하던 나날은 이제 없습니다.모든 플랫폼의 사진 치수를 기억해야 하던 나날은 이제 없습니다. 원하는 디자인을 정하고 나면 Adobe Express의 간편하고 마법 같은 크기 조절 기능으로 아무 소셜 플랫폼 규격으로 바꿀 수 있습니다. 프로젝트를 복제하고, 크기 조절을 누르고, 원하는 플랫폼을 선택하면 나머지는 AI가 알아서 합니다. 모든 채널의 콘텐츠를 순식간에 완성할 수 있습니다.",
+};
+const text_02 = {
+  title: "Ipsum",
+  paragraph:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+};
+const text_03 = {
+  title: "Why do we use it?",
+  paragraph:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+};
+// eslint-disable-next-line
+export default { text_01, text_02, text_03 };
