f5e680b31b15bb2dc6bd5e50a2cb60cc64410eba
남형석 <azerc@MacMini.local>
2022-08-11 12:51:01 +0900

    remove import unused images

--- src/Components/Carousel/CarouselSwiper.js
+++ src/Components/Carousel/CarouselSwiper.js
@@ -9,19 +9,6 @@ import "swiper/css/scrollbar";
 
 import ImageList from "../ImageList";
 
-import img_01 from "../../assets/images/beauty_01.jpg";
-import img_02 from "../../assets/images/beauty_02.jpg";
-import img_03 from "../../assets/images/beauty_03.jpg";
-import img_04 from "../../assets/images/beauty_04.jpg";
-import img_05 from "../../assets/images/beauty_05.jpg";
-import img_06 from "../../assets/images/beauty_06.jpg";
-import img_07 from "../../assets/images/beauty_07.jpg";
-import img_08 from "../../assets/images/beauty_08.jpg";
-import img_09 from "../../assets/images/beauty_09.jpg";
-import img_10 from "../../assets/images/beauty_10.jpg";
-import img_11 from "../../assets/images/beauty_11.jpg";
-import img_12 from "../../assets/images/beauty_12.jpg";
-
 const CarouselContainer = styled.div`
   margin: 0 auto;
   display: flex;
